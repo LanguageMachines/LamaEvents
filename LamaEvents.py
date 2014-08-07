54ba9ff901fbbb5dce9ff05a4f3880dec94a994e
ErkanBasar <mustafaerkanbasar@gmail.com>
2014-08-07 14:34:24 +0200

    Time Correction

--- LamaEvents.py
+++ LamaEvents.py
@@ -10,8 +10,8 @@ import DEvents.event_pairs as event_pairs
 
 #MongoLab Connection;
 connection = MongoClient(XXX)
-ledb = connection["XXX"]
-ledb.authenticate("XXX", "XXX")
+ledb = connection[XXX]
+ledb.authenticate(XXX)
 lecl = ledb.lecl
 
 ep = event_pairs.Event_pairs()
@@ -20,26 +20,30 @@ payload = {'SEARCH': 'echtalles', 'DATE': 'start', 'DOWNLOAD':True, 'SHOWTWEETS'
 
 while True:
     time.sleep(300)
-    a = datetime.datetime.now().strftime("20%y%m%d%H")
-    b = datetime.datetime.now().strftime("%H%M")
-    nowDATE = a+'-'+a
-    if payload['DATE'] == nowDATE:
+    nowDate = datetime.datetime.now()
+    nowDate_earlier = nowDate - datetime.timedelta(hours=2)
+    nes = nowDate_earlier.strftime("20%y%m%d%H")
+    pDATE = nes+'-'+nes
+    b = nowDate.strftime("%H%M")
+    if payload['DATE'] == pDATE:
         print(b)
         continue
     else:
-        payload['DATE'] = nowDATE
-        print('***'+payload['DATE'])
+        payload['DATE'] = pDATE
+        print("Tweet hour :"+payload['DATE'])
         #Request to Twiqs;
         output = requests.get("http://145.100.57.182/cgi-bin/twitter", params=payload, cookies={'cookie':'XXX'})
     #Add an automatic cookie finder here (requests.Session())
-        print("***Request Done.")
+        print("Request Completed")
         #Event Detection;
         EventDic = ep.detect_events(output.text[:-1]) # [:-1] = ignoring the last '\n' at the bottom of the file.
+        print("Event Detection Completed")
         #Date to Datetime;
         for k,v in EventDic.items():
             v['date'] = datetime.datetime.combine(v['date'], datetime.datetime.min.time())
             for i in v['tweets']:
                 i['date'] = datetime.datetime.combine(i['date'], datetime.datetime.min.time())
             lecl.insert(v) #Writing to Database
+        print("Written to Database")
     #Add here some break for  too long processes
         continue
