1c4bdce00adc6b6807440380234dd1d20bc9f493
ErkanBasar <mustafaerkanbasar@gmail.com>
2014-08-28 17:36:38 +0200

    Clean

--- dbcon/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class DaterForm(forms.Form):
-    start_date = forms.CharField(max_length=40, value='Start', required=True)
-    end_date = forms.CharField(max_length=40, required=True)
