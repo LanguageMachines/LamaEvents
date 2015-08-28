8ab5a0c19538f88dfd18c2c9779b16a0471aa3c6
Florian Kunneman <f.kunneman@gmail.com>
2015-08-28 12:04:42 +0200

    set django settings.py debug to True

--- LamaEvents/wsgi.py
+++ LamaEvents/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/scratch2/www/LamaEvents/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LamaEvents.settings")
 
 from django.core.wsgi import get_wsgi_application
