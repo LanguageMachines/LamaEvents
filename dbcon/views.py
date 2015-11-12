384e687acac9cbc08729728d5dd769b187aa15f8
Mustafa Erkan Basar <mustafaerkanbasar@gmail.com>
2015-11-12 16:03:38 +0100

    Calendar range reduced for mobile

--- dbcon/views.py
+++ dbcon/views.py
@@ -1,10 +1,14 @@
 """
 This is the standart views file for Django.  
+
+
 .. rubric:: Some Explanations
+
 With using 'timeIntstr_d' and 'timeIntstr_m' you can describe here how many dates will shown at 
 start and during navigation on calendar. '-1' is for making it look like time interval of 
 twiqs.nl (For one day you have to write the same date twice, ex : 10-09-2014 & 10-09-2014).
 '_d' stands for 'desktop' and '_m' stands for 'mobile'::
+
 	#Desktop;
 	timeIntstr_d = 6 
 	time_interval_d = timeIntstr_d - 1
@@ -12,7 +16,9 @@ twiqs.nl (For one day you have to write the same date twice, ex : 10-09-2014 & 1
 	timeIntstr_m = 2
 	time_interval_m = timeIntstr_m - 1
 	
+
 'is_mobile' is for saying what will happen if the device is mobile. Works with basic if statement. Also see LamaEvents/middleware.py::
+
 		if request.is_mobile:
 			timeIntstr = timeIntstr_m
 			time_interval = time_interval_m
@@ -21,14 +27,23 @@ twiqs.nl (For one day you have to write the same date twice, ex : 10-09-2014 & 1
 			timeIntstr = timeIntstr_d
 			time_interval = time_interval_d
 			template = 'desktop/nextint.html'
+
 The following code is used in many strftime & strptime convertions. It is important for the links with dates 
 because links are passing this date format to make queries. Therefore if you change it, you will change the urls. 
 Then you may be have to change the reqular expresion in the url.py too::
+
 	dateformat = "%d-%m-%Y"
+
+
 On the Applejack server, the link starts with '/lamaevents'. Because of that we defined a variable called urlprefix in settings.py.
 And here we are sending this variable to templates for adding to the links::
+
 		return render(request, template, {
 			'urlprefix': settings.URLPREFIX,
+
+
+
+
 :Contents:
 """
 
@@ -50,7 +65,7 @@ from django.conf import settings
 timeIntstr_d = 6 
 time_interval_d = timeIntstr_d - 1 
 
-timeIntstr_m = 6
+timeIntstr_m = 2
 time_interval_m = timeIntstr_m - 1
 
 
@@ -66,28 +81,37 @@ def call_dates(first_date, second_date):
 	"""
 	For creating the dates and events in calendar and binding them to each others. 
 	This is not a view! 
+
 	:param first_date: The Start Date
 	:param second_date: The End Date	
+
 	First it calculates the all dates between the start date and the end date. Then put them in 4 different lists as different values and zip them in one list;
 	
 		1. datelist = Used in the links to show events for one date. Resulted in dateitem.
 		2. dateliststr = Used to show dates as strings in format "%d %b %Y %a" (e.g.: 09 Sep 2014 Tue). Resulted in dateitemstr.
 		3. datetimelist = Used to query events. Items are in datetime format. Resulted in datetimeitem.
 		4. eventObjlist = Holds the events found by querying with items in datetimelist. Resulted in eventObj.
+
 	.. rubric::	Usage of call_dates()
+
 	In the views which we will use calendar there must be two exact dates. 
 	We need to call call_dates() method with using this dates as parameters and assign to a list::
  
 		allperEventsDictList = call_dates(now_date, dateLater) 
+
 		#And we have to render this to template;
 		return render(request, template, {
 			'allperEventsDictList': allperEventsDictList,
+
 	Then you can call them is template like this::
+
 		#Iterate on the allperEventsDictList. each element of allperEventsDictList is a dictionary whose keys identify dateitem, dateitemstr, eventObj, datetimeitem;
 		{% for x in allperEventsDictList %}
+
 			#eventObj is a list of events of a date. Thus, event is a Events class instance;
 			{% for event in x.eventObj %}
 					{{ event.score }}
+
 	"""
 
 	def daterange(first_date, second_date):
@@ -160,40 +184,62 @@ class Calendar(View):
 	def post(self, request):
 		"""
 		post() is used for user inputs. 
+
 		There are if statements inside to find out which button submitted;
+
 		1. Date Picker;
+
 			* This is working, if someone used 'Specificeer Datums' search.
 			* Assign the parameters to new string variables. These are also used for navigation with next or previous buttons::
+
 				start_date = request.POST['start_date']
 				end_date = request.POST['end_date']	
+
 			* Convert the strings to datetime for call_dates function::
+
 				startDate = datetime.strptime(start_date, dateformat)
 				endDate = datetime.strptime(end_date, dateformat)
 			
 				allperEventsDictList = call_dates(startDate, endDate)
+
 		:param start_date:
 		:param end_date:
+
+
 		2. Time-To-Event Estimation;
+
 			* This is working, if someone used "Specificeer Uren" search.
 			* It finds the events between these ranges. For example hour=100 range=20, it'll show the events between 80(start_hour) and 120(end_hour) hour after from now.
 			* If search hour is smaller than the range, it'll set the start hour to now. So it can't show before now::
+
 				if int(search_hour) > int(hour_range): 
 					start_hour = datetime.now() + timedelta(hours=(int(search_hour)-int(hour_range)))
 				else:
 					start_hour = datetime.now() 
+
 			* Find events which fits 'end_hour > event.Estimation > start_hour'::
+
 				event_list = Events.objects(Q(Estimation__gte = start_hour) & Q(Estimation__lte = end_hour)).order_by('Estimation')
+
 		:param search_hour:
 		:param hour_range: 
+
+
 		3. Event Search;
 	
 			* This is working, if someone used "Zoek met Zoekwoorden" search.
+
 			* It'll find the events according to keyterms they wrote. 
+
 			* The query finds events which have first or second keyterm and not in the past. 'iexact' means 'case insensitive'::
+
 				event_list = Events.objects()
 #				event_list = Events.objects(Q(Estimation__gte = start_hour) & Q(Estimation__lte = end_hour)).order_by('Estimation')
+
+
 		:param fst_key: First Keyterm
 		:param snd_key: Second Keyterm
+
 		"""
 
 		if "date_picker" in request.POST:
@@ -289,13 +335,16 @@ class IntervalSeek(View):
 	"""
 	If navigation links used, it will redirect the dates here and make a loop for dates.
 	So if navigation link used again right after the first time, it will come here again.
+
 	This is using call_dates() with the new dates coming from the links.
+
 	The date amount to show is defined with 'timeIntstr' again.
 	"""
 	def get(self, request, fst, snd):
 		"""
 		:param fst: First Day
 		:param snd: Last Day
+
 		These parameters also used to create new navigation links.
 		"""
 		if request.is_mobile:
@@ -334,9 +383,13 @@ class EventsofDate(View):
 	def get(self, request, dt):
 		"""
 		Finds the events for the selected date. dt comes from the url which is clicked.
+
 		Before making the query, change the string(dt) to datetime::
+
 			events_date_list = Events.objects(date=datetime.strptime(dt, dateformat)).order_by('-score')
+
 		:param dt: Comes from the links
+
 		"""
 		events_date_list = Events.objects(date=datetime.strptime(dt, dateformat)).order_by('-score')
 		
@@ -364,7 +417,9 @@ class EventDetail(View):
 	def get(self, request, id):
 		"""
 		Finds the exact event via id.
+
 		:param id: Comes from the links
+
 		"""
 		event = Events.objects.get(pk=id)
 
@@ -391,3 +446,6 @@ class About(View):
 		return render(request, template, {
 			'urlprefix': settings.URLPREFIX,
 		})
+
+
+
